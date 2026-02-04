78900322868ea819213e49c1130940a4a4f2d606
yangarch <gateflowers@gmail.com>
2026-02-04 13:42:18 +0900

    Fix path

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, String, Date, Float
-from .database import Base
+from database import Base
 from pydantic import BaseModel
 from datetime import date as date_type
 
